b6f78b205ef91b7667f8c9c505b84e30c7bf35e1
dnapog.base.eth <pierre-olivier.guichet@inserm.fr>
2025-11-30 21:59:28 +0100

    Create abi.ts

---
+++ config/abi.ts
@@ -0,0 +1,8 @@
+export const VAULT_ABI = [
+  {
+    "inputs": [
+      { "internalType": "uint256", "name": "unlockTime", "type": "uint256" },
+      { "internalType": "string", "name": "tokenURI", "type": "string" },
+      { "internalType": "bool", "name": "includeGasReserve", "type": "bool" }
+    ],
+    "name":
